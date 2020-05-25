562b4f08eac79aca0e44489f539233b69c49cbe1
Mityai Kowalksi <kovvalsking@gmail.com>
2020-05-25 17:08:54 +0300

    refactoring

--- src/Particle.js
+++ src/Particle.js
@@ -1,135 +1,115 @@
-class Particle {
-  constructor(imagePaths, config, type, useParticleContainer, stepColors) {
+class Controller {
+  constructor(imagePaths, config, type) {
     this.imagePaths = imagePaths;
     this.config = config;
     this.type = type;
-    this.useParticleContainer = useParticleContainer;
-    this.stepColors = stepColors;
-    this.elapsed = Date.now();
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
     this.setup();
-    this.preload();
   }
 
   setup() {
+    this.elapsed = Date.now();
     this.setupCanvas();
     this.clickHandler();
+    this.preload();
   }
 
   setupCanvas() {
-    this.canvas = new PIXI.Application(window.innerWidth, window.innerHeight);
-    document.body.appendChild(this.canvas.view);
-    this.canvas.width = window.innerWidth;
-    this.canvas.height = window.innerHeight;
+    this.createCanvas();
+    this.createRenderer();
+  }
 
-    const rendererOptions = {
-      view: this.canvas.view,
-    };
+  createCanvas() {
+    this.app = new PIXI.Application();
+    document.body.appendChild(this.app.view);
+    this.app.width = this.width;
+    this.app.height = this.height;
+  }
 
+  createRenderer() {
     this.stage = new PIXI.Container();
-    this.emitter = null;
-    this.renderer = PIXI.autoDetectRenderer(
-      this.canvas.width,
-      this.canvas.height,
-      rendererOptions
-    );
-  }
-
-  setupSpine(loader, res) {
-    this.bigwin = new PIXI.spine.Spine(res.bigwin.spineData);
-    this.bigwin.skeleton.setToSetupPose();
-    this.bigwin.autoUpdate = false;
-
-    this.bigwinContainer = new PIXI.Container();
-    this.bigwinContainer.addChild(this.bigwin);
-
-    this.localRect = this.bigwin.getLocalBounds();
-    this.bigwin.position.set(-this.localRect.x, -this.localRect.y);
-
-    this.bigwinContainer.scale.set(1);
-    this.bigwinContainer.position.set(
-      window.innerWidth * 0.5,
-      window.innerHeight * 0.5
-    );
-
-    this.stage.addChild(this.bigwinContainer);
-
-    this.bigwin.state.setAnimation(0, "big_win_all", true);
-  }
-
-  update() {
-    window.requestAnimationFrame.bind(window, this.update.bind(this))();
-    const framerate = document.getElementById("framerate");
-    const now = Date.now();
-    this.bigwin.update((now - this.elapsed) * 0.001);
-    this.emitter.update((now - this.elapsed) * 0.001);
-
-    framerate.innerHTML = (1000 / (now - this.elapsed)).toFixed(2);
-
-    this.elapsed = now;
-
-    this.renderer.render(this.stage);
+    this.renderer = PIXI.autoDetectRenderer(this.app.width, this.app.height, {
+      view: this.app.view,
+    });
   }
 
   preload() {
-    const urls = [this.imagePaths.spritesheet];
-    urls.push("images/bc.png");
     const loader = PIXI.loader;
+    this.addToLoader(loader);
+
+    loader.load((_, res) => {
+      this.onAssetsLoaded(_, res);
+    });
+  }
+
+  addToLoader(loader) {
+    const urls = [this.imagePaths.spritesheet];
     loader.add("bigwin", "bigwin/big_win_animation.json");
     urls.forEach((url, idx) => {
       loader.add(`img${idx}`, url);
     });
+  }
 
-    loader.load((loader, res) => {
-      this.setupSpine(loader, res);
-      const art = this.imagePaths.art;
-      let emitterContainer;
-      if (this.useParticleContainer) {
-        emitterContainer = new PIXI.ParticleContainer();
-        emitterContainer.setProperties({
-          scale: true,
-          position: true,
-          rotation: true,
-          uvs: true,
-          alpha: true,
-        });
-      } else {
-        emitterContainer = new PIXI.Container();
-      }
+  onAssetsLoaded(_, res) {
+    this.setupSpine(_, res);
+    this.setupParticle();
 
-      this.stage.addChild(emitterContainer);
+    this.update();
+  }
 
-      window.emitter = this.emitter = new PIXI.particles.Emitter(
-        emitterContainer,
-        art,
-        config
-      );
+  setupSpine(_, res) {
+    this.bigwin = new PIXI.spine.Spine(res.bigwin.spineData);
+    this.bigwin.skeleton.setToSetupPose();
+    this.bigwin.autoUpdate = false;
 
-      if (this.type === "path") {
-        this.emitter.particleConstructor = PIXI.particles.PathParticle;
-      } else if (this.type === "anim") {
-        this.emitter.particleConstructor = PIXI.particles.AnimatedParticle;
-      }
+    const bigwinContainer = new PIXI.Container();
+    bigwinContainer.addChild(this.bigwin);
 
-      emitter.updateOwnerPos(window.innerWidth / 2, window.innerHeight / 2);
-      this.update();
-    });
+    this.localRect = this.bigwin.getLocalBounds();
+    this.bigwin.position.set(-this.localRect.x, -this.localRect.y);
+
+    bigwinContainer.scale.set(1);
+    bigwinContainer.position.set(this.width / 2, this.height / 2);
+
+    this.stage.addChild(bigwinContainer);
+    this.bigwin.state.setAnimation(0, "big_win_all", true);
+  }
+
+  setupParticle() {
+    const art = this.imagePaths.art;
+    const emitterContainer = new PIXI.Container();
+
+    this.stage.addChild(emitterContainer);
+
+    this.emitter = new PIXI.particles.Emitter(emitterContainer, art, config);
+    this.emitter.particleConstructor = PIXI.particles.AnimatedParticle;
+    this.emitter.updateOwnerPos(this.width / 2, this.height / 2);
+  }
+
+  update() {
+    window.requestAnimationFrame.bind(window, this.update.bind(this))();
+
+    const framerate = document.getElementById("framerate");
+    const now = Date.now();
+    const delta = now - this.elapsed;
+    const frequency = delta * 0.001;
+    const fps = 1000 / delta;
+
+    this.bigwin.update(frequency);
+    this.emitter.update(frequency);
+
+    framerate.innerHTML = `FPS: ${fps.toFixed(2)}`;
+    this.elapsed = now;
+    this.renderer.render(this.stage);
   }
 
   clickHandler() {
-    this.canvas.view.addEventListener("mouseup", (e) => {
-      if (!emitter) return;
+    this.app.view.addEventListener("mouseup", (e) => {
+      if (!this.emitter) return;
       this.emitter.emit = true;
       this.emitter.resetPositionTracking();
       this.emitter.updateOwnerPos(e.offsetX || e.layerX, e.offsetY || e.layerY);
     });
   }
-
-  setupBG() {
-    const bg = new PIXI.Sprite(PIXI.Texture.fromImage("images/bc.png"));
-    bg.width = this.canvas.width;
-    bg.height = this.canvas.height;
-    this.stage.addChild(bg);
-  }
 }
-
-window.Particle = Particle;
--- src/index.js
+++ src/index.js
@@ -1,40 +1,139 @@
-var app = new PIXI.Application(window.innerWidth, window.innerHeight);
-document.body.appendChild(app.view);
+const images = {
+  spritesheet: "coin.json",
+  art: [
+    {
+      framerate: 30,
+      loop: false,
+      textures: [
+        "0001.png",
+        "0002.png",
+        "0003.png",
+        "0004.png",
+        "0005.png",
+        "0006.png",
+        "0007.png",
+        "0008.png",
+        "0009.png",
+        "0010.png",
+        "0011.png",
+        "0012.png",
+        "0013.png",
+        "0014.png",
+        "0015.png",
+        "0016.png",
+        "0017.png",
+        "0018.png",
+        "0019.png",
+        "0020.png",
+        "0021.png",
+        "0022.png",
+        "0023.png",
+        "0024.png",
+        "0025.png",
+        "0026.png",
+        "0027.png",
+        "0028.png",
+        "0029.png",
+        "0030.png",
+      ],
+    },
+    {
+      framerate: 30,
+      loop: false,
+      textures: [
+        "0030.png",
+        "0029.png",
+        "0028.png",
+        "0027.png",
+        "0026.png",
+        "0025.png",
+        "0024.png",
+        "0023.png",
+        "0022.png",
+        "0021.png",
+        "0020.png",
+        "0019.png",
+        "0018.png",
+        "0017.png",
+        "0016.png",
+        "0015.png",
+        "0014.png",
+        "0013.png",
+        "0012.png",
+        "0011.png",
+        "0010.png",
+        "0009.png",
+        "0008.png",
+        "0007.png",
+        "0006.png",
+        "0005.png",
+        "0004.png",
+        "0003.png",
+        "0002.png",
+        "0001.png",
+      ],
+    },
+  ],
+};
 
-app.stop();
+const config = {
+  alpha: {
+    start: 1,
+    end: 1,
+  },
+  scale: {
+    start: 0.45,
+    end: 0.45,
+    minimumScaleMultiplier: 0.5,
+  },
+  color: {
+    start: "#ffffff",
+    end: "#f9fcc0",
+  },
+  speed: {
+    list: [
+      {
+        value: 500,
+        time: 0,
+      },
+      {
+        value: 0,
+        time: 0.1,
+      },
+      {
+        value: 0,
+        time: 1,
+      },
+    ],
+    isStepped: false,
+    minimumSpeedMultiplier: 2.25,
+  },
+  acceleration: {
+    x: 0,
+    y: 2000,
+  },
+  startRotation: {
+    min: 0,
+    max: 360,
+  },
+  rotationSpeed: {
+    min: 100,
+    max: 200,
+  },
+  lifetime: {
+    min: 1.5,
+    max: 1.5,
+  },
+  blendMode: "normal",
+  frequency: 0.001,
+  emitterLifetime: 0.5,
+  maxParticles: 70,
+  pos: {
+    x: 0,
+    y: 0,
+  },
+  addAtBack: true,
+  spawnType: "point",
+};
 
-// load spine data
-PIXI.loader.add("bigwin", "bigwin/big_win_animation.json").load(onAssetsLoaded);
-
-var dragon = null;
-
-function onAssetsLoaded(loader, res) {
-  // instantiate the spine animation
-  dragon = new PIXI.spine.Spine(res.bigwin.spineData);
-  dragon.skeleton.setToSetupPose();
-  dragon.autoUpdate = false;
-
-  // create a container for the spine animation and add the animation to it
-  var dragonCage = new PIXI.Container();
-  dragonCage.addChild(dragon);
-
-  // measure the spine animation and position it inside its container to align it to the origin
-  var localRect = dragon.getLocalBounds();
-  dragon.position.set(-localRect.x, -localRect.y);
-
-  dragonCage.scale.set(1);
-  dragonCage.position.set(window.innerWidth * 0.5, window.innerHeight * 0.5);
-
-  // add the container to the stage
-  app.stage.addChild(dragonCage);
-
-  // once position and scaled, set the animation to play
-  dragon.state.setAnimation(0, "big_win_all", true);
-
-  app.start();
-}
-
-app.ticker.add(function () {
-  // update the spine animation, only needed if dragon.autoupdate is set to false
-  dragon.update(0.01666666666667); // HARDCODED FRAMERATE!
-});
+new Controller(images, config, "anim");
